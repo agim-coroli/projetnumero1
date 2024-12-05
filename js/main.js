047eed4389cd8783fd4f31c1f5fa51070a1dba3a
Agim Coroli <agim.coroli8016@gmail.com>
2024-12-05 22:20:06 +0100

    demain tu va faire ton carousel tout seul batard

--- js/main.js
+++
@@ -1,7 +0,0 @@
-let prev = document.getElementById('carousel__prev')
-let image = document.getElementById("imageToDisplay");
-let next = document.getElementById('carousel__next')
-
-prev.addEventListener('click', function() {
-    image.style.display = 'block'
-})
