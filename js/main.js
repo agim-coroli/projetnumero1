970affad45ce633c00efa70381ce7adbf3ae9348
agim <agim.coroli8016@gmail.com>
2024-12-06 13:41:21 +0100

    finition carousel

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let images = document.querySelectorAll('.main__carousel img');
+let nextButton = document.getElementById('carousel__next')
+let prevButton = document.getElementById('carousel__prev')
+
+var currentIndex = 0;
+
+// mise a jour de lemplacement de l'index 
+function update() {
+    images.forEach((img, i) => {
+        if (currentIndex === i) {
+            img.style.display = "block"
+        } else {
+            img.style.display = "none"
+        }
+    });
+}
+
+nextButton.addEventListener('click', function()  {
+    currentIndex = (currentIndex + 1) % images.length
+    update()
+})
+
+prevButton.addEventListener('click', function()  {
+    currentIndex = (currentIndex + 4) % images.length
+    update()
+})
+
+update()
